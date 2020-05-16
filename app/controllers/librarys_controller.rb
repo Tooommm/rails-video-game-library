bb53a7c55d86920082baeae988b72b291392704e
Thomas Brouillet <thomasbrouillet@gmail.com>
2020-05-16 22:20:54 +0200

    models controllers and view for user and library

--- app/controllers/librarys_controller.rb
+++ app/controllers/librarys_controller.rb
@@ -1,2 +1,35 @@
 class LibrarysController < ApplicationController
+  def index
+    @librarys = Library.all
+  end
+
+  def new
+    @user = User.find((params[:user_id]))
+    @library = Library.new
+  end
+
+  def show
+    @library = library.find(params[:id])
+  end
+
+  def create
+  @library = Library.new(library_params)
+  if @library.save
+    redirect_to library_path(@library)
+  else
+    render :new
+  end
+  end
+
+  def destroy
+    @library = Library.find(params[:id])
+    @library.destroy
+    redirect_to librarys_path
+  end
+
+  private
+
+  def library_params
+    params.require(:library).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  resources :users do
+    resources :librarys
+  end
+
+  resources :games
+  resources :categorys
+  resources :support
+  root 'users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
